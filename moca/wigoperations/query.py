c862f72011198774b338c0004db1798ba4ee2f24
Saket Choudhary <saketkc@gmail.com>
2016-02-23 11:23:15 -0800

    Add wigoperators

--- moca/wigoperations/query.py
+++ moca/wigoperations/query.py
@@ -1,1 +1,50 @@
+import pyBigWig
+from ..helpers import MocaException
+
+class WigReader(object):
+    """Class for reading and querying wigfiles"""
+    def __init__(self, wig_location):
+        """
+        Arguments
+        ---------
+        wig_location: Path to bigwig
+
+        """
+        self.wig_location = wig_location
+        try:
+            self.wig = pyBigWig.open(self.wig_location)
+        except Exception as e:
+            raise MocaException('Error reading wig file: {}'.format(e))
+
+    def query(self, intervals):
+        """ Query regions for scores
+
+        Arguments
+        ---------
+        intervals: list of tuples
+            A list of tuples with the following format: (chr, chrStart, chrEnd)
+
+        Returns
+        -------
+        scores: list of lists
+            A list of lists containing scores for each tuple
+        """
+        scores = []
+        for chrom, chromStart, chromEnd in intervals:
+            score = self.wig.values(chrom, chromStart, chromEnd)
+            scores.append(score)
+        return scores
+
+    def _get_chromosomes(self):
+        """Return list of chromsome and their sizes
+        as in the wig file
+
+        Returns
+        -------
+        chroms: dict
+            Dictionary with {"chr": "Length"} format
+
+        """
+        return self.wig.chroms()
+
 
---
+++ tests/test_wigoperations.py
@@ -0,0 +1,28 @@
+"""
+test_wigoperations
+----------------------------------
+
+Tests for `moca.wigoperations` module.
+"""
+
+from moca.wigoperations import WigReader
+import unittest
+from moca.helpers import MocaException
+from array import array
+from math import isnan
+
+class TestWigoperations(unittest.TestCase):
+
+    def setUp(self):
+        self.wig_location = 'tests/data/test.bw'
+
+    def test_open(self):
+        loaded_wig = WigReader(self.wig_location)
+        assert(loaded_wig is not None)
+
+    def test_query(self):
+        loaded_wig = WigReader(self.wig_location)
+        scores = loaded_wig.query([("1", 0, 3),("1",150,151)])
+        assert scores == [[0.10000000149011612, 0.20000000298023224, 0.30000001192092896], [1.5]]
+
+
