b491d3e9261599176a979366825534e4b5d4352a
Saket Choudhary <saketkc@gmail.com>
2016-02-29 12:05:35 -0800

    TST: Compare meme output for tests instead of exit code

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -11,6 +11,7 @@ import unittest
 from Bio import SeqIO
 from moca.pipeline import Pipeline
 from moca.bedoperations import fimo_to_sites
+import filecmp
 
 class TestPipeline(unittest.TestCase):
     """Test pipeline """
@@ -28,14 +29,21 @@ class TestPipeline(unittest.TestCase):
         output = self.pipeline.run_meme(fasta_in=self.fasta, out_dir='tests/data/out/meme_analysis')
         #TODO Check if meme.txt is same and created
         print output
-        assert output['exitcode'] == 0
+        #TODO This check is too stringent, specially if logos are being produced.
+        #MEME installation leads to hard coded paths
+        #assert output['exitcode'] == 0
+        assert filecmp.cmp('tests/data/out/meme_analysis/meme.txt',
+                           'tests/data/meme_analysis/meme.txt')
         if os.path.exists('tests/data/out/meme_analysis'):
             shutil.rmtree('tests/data/out/meme_analysis')
 
     def test_fimo(self):
         """Test fimo runner"""
-        fimo_output = self.pipeline.run_fimo(motif_file='tests/data/meme_analysis/meme.txt', motif_num=1,
-                                             sequence_file=self.fasta, out_dir=None, strargs=None)
+        fimo_output = self.pipeline.run_fimo(motif_file='tests/data/meme_analysis/meme.txt',
+                                             motif_num=1,
+                                             sequence_file=self.fasta,
+                                             out_dir=None,
+                                             strargs=None)
         assert fimo_output['exitcode'] == 0
 
     def test_fimo_to_sites(self):
@@ -50,8 +58,8 @@ class TestPipeline(unittest.TestCase):
             strand = row['strand']
             fasta_sequence = record_dict[record_id].seq
             if strand == '+':
-               assert str(fasta_sequence)[start:end] == fimo_sequence
+                assert str(fasta_sequence)[start:end] == fimo_sequence
             else:
-               assert str(fasta_sequence[start:end].reverse_complement()) == fimo_sequence
+                assert str(fasta_sequence[start:end].reverse_complement()) == fimo_sequence
 
 
