305e5a17674ff8f1d07c55954749caef2e2652f6
Saket Choudhary <saketkc@gmail.com>
2016-03-08 12:05:53 -0800

    Add moca cli

---
+++ scripts/mocacli.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""MoCA CLI"""
+from __future__ import print_function
+import os
+import sys
+import numpy as np
+import click
+from progressbar import ProgressBar, Bar, Percentage, AnimatedMarker, Timer, ETA
+from moca import bedoperations, wigoperations, pipeline
+from moca.bedoperations import fimo_to_sites
+from moca.helpers import filename_extension
+from moca.helpers import generate_random_fasta
+from moca.helpers import read_memefile
+from moca.helpers import get_fasta_metadata
+from moca.helpers.job_executor import safe_makedir
+
+STEPS = 0
+bar = None
+
+def save_score(directory, phylop_wig, gerp_wig):
+    fimo_file = os.path.join(directory, 'fimo.txt')
+    fimo_sites = fimo_to_sites(fimo_file)
+
+    subset = fimo_sites[['chrom', 'motifStartZeroBased', 'motifEndOneBased', 'strand']]
+    intervals = [tuple(x) for x in subset.to_records(index=False)]
+
+    scores_phylop = phylop_wig.query(intervals)
+    scores_gerp = gerp_wig.query(intervals)
+
+    scores_phylop_mean = np.mean(scores_phylop, axis=0)
+    scores_gerp_mean = np.mean(scores_gerp, axis=0)
+
+    np.savetxt(os.path.join(directory, 'phylop.raw.txt'), scores_phylop, fmt='%.4f')
+    np.savetxt(os.path.join(directory, 'gerp.raw.txt'), scores_gerp, fmt='%.4f')
+
+    np.savetxt(os.path.join(directory, 'phylop.mean.txt'), scores_phylop_mean, fmt='%.4f')
+    np.savetxt(os.path.join(directory, 'gerp.mean.txt'), scores_gerp_mean, fmt='%.4f')
+
+def show_progress(msg):
+    global STEPS
+    STEPS += 1
+    print(msg)
+    bar.update(STEPS)
+
+@click.command()
+@click.option('--bedfile', help='Bed file input')
+@click.option('--phylop', help='phlyop file input', required=True)
+@click.option('--gerp', help='Gerp file input', required=True)
+@click.option('--configuration', help='Configuration file', required=True)
+@click.option('--genome-table', '-gt', help='Chromosome size file', required=True)
+@click.option('--genome-fasta', '-gf', help='Genome fasta', required=True)
+@click.option('--flank-length', default=100, help='Flanking sequence length', required=True)
+
+def mocacli(bedfile, genome_table, genome_fasta,
+            flank_length, configuration, phylop, gerp):
+    """Run moca"""
+    global bar
+    root_dir = os.path.dirname(os.path.abspath(bedfile))
+    bar = ProgressBar(widgets=[Percentage(), Bar(), ETA(), Timer()], max_value=4+3*5).start()
+    moca_out_dir = os.path.join(root_dir, 'moca_output')
+    safe_makedir(moca_out_dir)
+    bedfile_fn, _ = filename_extension(bedfile)
+    query_fasta = os.path.join(moca_out_dir, bedfile_fn + '_flank_{}.fasta'.format(flank_length))
+    bed_df = bedoperations.Bedfile(bedfile, genome_table)
+    bed_df.determine_peaks()
+    bed_df.slop_bed(flank_length=flank_length)
+    show_progress('Extract Fasta')
+    bed_df.extract_fasta(fasta_in=genome_fasta, fasta_out=query_fasta)
+
+    moca_pipeline = pipeline.Pipeline(configuration)
+    meme_out_dir = os.path.join(moca_out_dir, 'meme_analysis')
+    show_progress('Run meme')
+    meme_run_out = moca_pipeline.run_meme(fasta_in=query_fasta, out_dir=meme_out_dir)
+
+    meme_file = os.path.join(meme_out_dir, 'meme.txt')
+    meme_summary = read_memefile(meme_file)
+    fasta_metadata = get_fasta_metadata(query_fasta)
+
+    show_progress('Read Phylop')
+    phylop_wig = wigoperations.WigReader(phylop)
+    show_progress('Read Gerp')
+    gerp_wig = wigoperations.WigReader(gerp)
+
+    for motif in range(1, meme_summary['total_motifs']+1):
+        fimo_rand_dir = os.path.join(moca_out_dir, 'motif_{}_fimo_analysis_random'.format(motif))
+        fimo_main_dir = os.path.join(moca_out_dir, 'motif_{}_fimo_analysis_main'.format(motif))
+        safe_makedir(fimo_rand_dir)
+        safe_makedir(fimo_main_dir)
+        random_fasta = os.path.join(fimo_rand_dir, 'random_{}.fa'.format(motif))
+        show_progress('Generate Random Fasta: {}'.format(motif))
+        ##TODO: This step takes 4 minutes!! This is the least efficient step!
+        #generate_random_fasta(genome_fasta,
+        #                      genome_table,
+        #                      fasta_metadata['num_seq'],
+        #                      fasta_metadata['len_seq'],
+        #                      random_fasta)
+
+        #Random
+        show_progress('Run Fimo Random')
+        fimo_rand = moca_pipeline.run_fimo(motif_file=meme_file,
+                                           motif_num=motif,
+                                           sequence_file=random_fasta,
+                                           out_dir=fimo_rand_dir)
+        #bar.update(item_id=u'Run fimo rand', force_flush=True)
+        #MainMotif
+        show_progress('Run Fimo Main')
+        fimo_main = moca_pipeline.run_fimo(motif_file=meme_file,
+                                           motif_num=motif,
+                                           sequence_file=query_fasta,
+                                           out_dir=fimo_main_dir)
+
+        show_progress('Process Scores Random')
+        save_score(fimo_rand_dir, phylop_wig, gerp_wig)
+        show_progress('Process Scores Main')
+        save_score(fimo_main_dir, phylop_wig, gerp_wig)
+    bar.finish()
+if __name__ == '__main__':
+    mocacli()
+
