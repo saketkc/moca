95bf92638612f514790cc027442640c73b44d6fc
Saket Choudhary <saketkc@gmail.com>
2016-05-21 19:49:55 -0700

    Modularize

--- moca/webservice/api/api.py
+++ moca/webservice/api/api.py
@@ -13,6 +13,20 @@ def api_response(data, success, message):
 
     }
 
+#TODO Fix this and the method below
+
+def db_connector(configuration_file):
+    config = ConfigurationParser(configuration_file)
+    mongo_config = config.get_section('mongo')
+    mongo_client = MongoClient('mongodb://{}:{}@{}/{}'.format(mongo_config['mongo_username'],
+                                                              mongo_config['mongo_password'],
+                                                              mongo_config['mongo_host'],
+                                                              mongo_config['mongo_dbname']),
+                                    port=int(mongo_config['mongo_port']))
+    db = mongo_client.moca_encode_tf
+    collection = db[mongo_config['mongo_collection']]
+    return collection
+
 class Webservice(Resource):
     """Base class to implement API"""
     def __init__(self, configuration_file):
@@ -23,15 +37,7 @@ class Webservice(Resource):
         configuration_file: str
             Path to configuration file
         """
-        self.config = ConfigurationParser(configuration_file)
-        self.mongo_config = self.config.get_section('mongo')
-        self.mongo_client = MongoClient('mongodb://{}:{}@{}/{}'.format(self.mongo_config['mongo_username'],
-                                                                    self.mongo_config['mongo_password'],
-                                                                    self.mongo_config['mongo_host'],
-                                                                    self.mongo_config['mongo_dbname']),
-                                        port=int(self.mongo_config['mongo_port']))
-        self.db = self.mongo_client.moca_encode_tf
-        self.collection = self.db[self.mongo_config['mongo_collection']]
+        self.collection = db_connector(configuration_file)
 
         self.columns_to_return = ['encode_id',
                                   'motif_number',
@@ -44,36 +50,16 @@ class Webservice(Resource):
                                   'r_phylop_sample_pval',
                                   'r_gerp_sample',
                                   'r_gerp_sample_pval',
-                                  'motif_evalue',
-                                  'motif_logo',
-                                  'motif_logorc',
-                                  'moca_plot',
-                                  'moca_plotrc']
+                                  'motif_evalue']
 
     def get(self):
         """Get all records from collection"""
-        data = list(self.collection.find())
+        data = list(self.collection.find({}, dict((k,1) for k in self.columns_to_return)))
         for d in data:
             d.update((k, "NaN") for k, v in d.iteritems() if str(v).lower()=='nan')
-            temp_d = {}
-            for k in self.columns_to_return:
-                if k not in d:
-                    temp_d[k] = 'NAN'
-                else:
-                    temp_d[k]= d[k]
-            keys = d.keys()
-            for key in keys:
-                if key not in self.columns_to_return:
-                    del d[key]
-            d.update(temp_d)
             d.update((k, float('%.2e' % v)) for k,v in d.iteritems() if 'pval' in k and type(v)==float)
             d.update((k, round(v,3)) for k,v in d.iteritems() if 'pval' not in k and type(v)==float)
-
-            #for key in d.keys():
-            #    if key in ['gerp_mean_sample', 'gerp_mean_control', 'phylop_mean_sample', 'phylop_mean_control']:
-            #        del d[key]
-
-        print data[0]
+            del d['_id']
         aoColumns = []
         for key in self.columns_to_return:
             aoColumns.append({"mDataProp": key, "sTitle": key})
@@ -82,28 +68,34 @@ class Webservice(Resource):
                             success="success",
                             message="All records")
 
-    def get_filtered_records(self, filter_dict=None):
-        """Get records post filtering
+
+class GetPlot(Resource):
+    def __init__(self, configuration_file):
+        """Init
 
         Parameters
         ----------
-
-        filter_dict: dict
-            A dictionary with key=filter key,
-
+        configuration_file: str
+            Path to configuration file
         """
-        pass
+        self.collection = db_connector(configuration_file)
 
+    def get(self, encode_id, motif_number):
+        data = list(self.collection.find({'encode_id': encode_id,
+                                          'motif_number': motif_number}, {'moca_plot': 1, '_id':0}))
+        return api_response(data={'rows': data, 'columns': {}},
+                            success="success",
+                            message="All records")
 
 
 if __name__ == '__main__':
     if len(sys.argv)!=2:
         print  'Run: python api.py <configuration.cfg>'
         sys.exit(1)
-    print sys.argv[1]
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(Webservice, '/', resource_class_args = [sys.argv[1]])
+    api.add_resource(GetPlot, '/plot/<string:encode_id>/<int:motif_number>', resource_class_args = [sys.argv[1]])
     app.run(host='moca.usc.edu',
             debug=True,
             port=8889)
--- moca/webservice/client/client.py
+++ moca/webservice/client/client.py
@@ -10,7 +10,6 @@ def init_params(configuration_file):
     config = ConfigurationParser(configuration_file)
     server_config = config.get_section('server')
     server_config = dict( (key.upper(), value) for key, value in server_config.iteritems())
-    print server_config
     for key, value in server_config.iteritems():
         try:
             # For integer
@@ -23,6 +22,14 @@ def encode_results():
     response = requests.get('http://moca.usc.edu:8889')
     data = response.json()
     return render_template('encodeanalysis.html', results=data)
+
+@app.route('/plot/<string:encode_id>/<int:motif_number>')
+def get_plot(encode_id, motif_number):
+    response = requests.get('http://moca.usc.edu:8889/plot/{}/{}'.format(encode_id, motif_number))
+    data = response.json()
+    return jsonify(data)
+
+
 
 if __name__ == '__main__':
     init_params(sys.argv[1])
