c5f412c1cdc784ce31c5f2035986cb0b5a71fad1
Saket Choudhary <saketkc@gmail.com>
2016-05-28 20:14:00 -0700

    TST: Cache meme dir

--- travis/install_meme.sh
+++ travis/install_meme.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 cd ~
-wget http://meme-suite.org/meme-software/4.10.2/meme_4.10.2.tar.gz
+wget -c --quiet http://meme-suite.org/meme-software/4.10.2/meme_4.10.2.tar.gz
 tar -zxvf meme_4.10.2.tar.gz 1>/dev/null 2>/dev/null
 cd meme_4.10.2
 ./configure --prefix="$HOME/meme_bin" 1>/dev/null 2>/dev/null
 make install 1>/dev/null 2>/dev/null
 cd ~
-wget http://meme-suite.org/meme-software/4.11.0/meme_4.11.0.tar.gz
+wget -c --quiet http://meme-suite.org/meme-software/4.11.0/meme_4.11.0.tar.gz
 tar -zxvf meme_4.11.0.tar.gz 1>/dev/null 2>/dev/null
 cd meme_4.11.0
 ./configure --prefix="$HOME/meme_new" 1>/dev/null 2>/dev/null
