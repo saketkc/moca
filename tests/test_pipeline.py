9d868c370e24919a3ab29473c8980aeee95261d6
Saket Choudhary <saketkc@gmail.com>
2017-02-01 10:51:37 -0800

    Fix test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -35,7 +35,7 @@ class TestPipeline(unittest.TestCase):
         #TODO Check if meme.txt is same and created
         #TODO This check is too stringent, specially if logos are being produced.
         #MEME installation leads to hard coded paths
-        print output
+        print(output)
         assert output['exitcode'] == 0
         meme_record = read_memefile('tests/data/generated_out/meme_analysis/meme.txt')
         assert meme_record['total_motifs'] == 5
